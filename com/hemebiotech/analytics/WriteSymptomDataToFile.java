e8dba2274bd9a7a14049f32f0b8027dbbb5e3fe2
Badr Benmammar <5n40bg43wsc9-student@anonymous-codio.com>
2023-04-20 22:28:25 +0000

    writer implementation

--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,8 +1,33 @@
 package com.hemebiotech.analytics;
 
+
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
+import java.io.PrintWriter;
 import java.util.Map;
 
+
 // WRITE YOUR CODE HERE
+
+public class WriteSymptomDataToFile implements ISymptomWriter {
+
+	@Override
+	public void writeSymptoms(Map<String, Integer> symptoms) { 
+            
+             try ( 
+              PrintWriter pw = new PrintWriter(new FileWriter("result.out"))) {
+          // browse through the table and record the results in the file
+          
+ // sortie of type Map.Entry<String, Integer>          
+          for (Map.Entry<String, Integer> sortie : symptoms.entrySet()) { // entrySet() to obtain a set 
+            
+              pw.println(sortie.getKey() + " : " + sortie.getValue());
+          } 
+      }     catch (IOException ex) {
+              
+            }
+    
+    System.out.println("Résultat sauvegardé dans le fichier 'result.out'.");
+	
+	}
+}
