d08c892c3d4cb2e1f385f4f50a7a6408efdfb1f7
Badr Benmammar <5n40bg43wsc9-student@anonymous-codio.com>
2023-04-21 20:52:38 +0000

    refactor analytics counter

--- com/hemebiotech/analytics/AnalyticsCounter.java
+++ com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,19 +1,14 @@
 package com.hemebiotech.analytics;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.TreeMap;
 
 public class AnalyticsCounter {
  
-    private ISymptomReader reader;
-    private ISymptomWriter writer;
+    private final ISymptomReader reader;
+    private final ISymptomWriter writer;
     
 public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) { 
     this.reader=reader;
@@ -27,7 +22,7 @@ public List<String> getSymptoms() {
 public Map<String, Integer> countSymptoms(List<String> symptoms) {
     HashMap<String, Integer> map = new HashMap<>();
     symptoms.forEach(symptom -> {
-        map.compute(symptom, (key, value) -> (value == null) ? 1 : value + 1);
+       ajouter (map,symptom);
         });
 return map;
 }
@@ -39,32 +34,9 @@ public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
 public void writeSymptoms(Map<String, Integer> symptoms) { 
 writer.writeSymptoms(symptoms);
 }
-  
-  public static void main(String args[]) throws IOException {
-   
-    HashMap<String, Integer> map = new HashMap<>();
-      try (Scanner a = new Scanner(new File("symptoms.txt"))) {
-          while (a.hasNextLine()) { // there are lines that have not been read   
-              String ligne = a.nextLine().trim(); // read the file line by line
-              if (!ligne.isEmpty()) ajouter(map, ligne); // add the line that is not empty in the HashMap 
-          } }
-   
-      try ( 
-              PrintWriter pw = new PrintWriter(new FileWriter("result.out"))) {
-          // browse through the table and record the results in the file
-          
- // sortie of type Map.Entry<String, Integer>          
-          for (Map.Entry<String, Integer> sortie : map.entrySet()) { // entrySet() to obtain a set 
-            
-              pw.println(sortie.getKey() + " : " + sortie.getValue());
-          } 
-      }
     
-    System.out.println("Résultat sauvegardé dans le fichier 'result.out'.");
-  }
-  
-  static void ajouter(Map<String, Integer> map, String phrase) {
-map.compute(phrase, (key, value) -> (value == null) ? 1 : value + 1);
+  static void ajouter(Map<String, Integer> map, String symptom) {
+map.compute(symptom, (key, value) -> (value == null) ? 1 : value + 1);
 }
   
 }
