3bc5a8867a2b92a280db435333ef0be845ce1182
Badr Benmammar <5n40bg43wsc9-student@anonymous-codio.com>
2023-04-21 21:00:36 +0000

    refactor analytics counter

--- com/hemebiotech/analytics/Main.java
+++ com/hemebiotech/analytics/Main.java
@@ -1,20 +1,10 @@
+
+
 package com.hemebiotech.analytics;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 // WRITE YOUR CODE HERE
 
 public class Main {
-    public static void main(String args[]) throws IOException {
-        
-       ISymptomReader r= new ReadSymptomDataFromFile ("symptoms.txt");
-      ISymptomWriter w= new WriteSymptomDataToFile ();
-      AnalyticsCounter a = new AnalyticsCounter (r, w);
-      List<String> liste = a.getSymptoms();
-     Map<String, Integer> map=a.countSymptoms(liste);
-     map =a.sortSymptoms (map);
-     a.writeSymptoms(map);     
-    }   
+
 }
