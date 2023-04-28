2e7ae01501a1961c62b73a8fd66a640afb61f1a9
Badr Benmammar <5n40bg43wsc9-student@anonymous-codio.com>
2023-04-28 20:46:17 +0000

    final code

--- com/hemebiotech/analytics/ISymptomReader.java
+++ com/hemebiotech/analytics/ISymptomReader.java
@@ -8,6 +8,7 @@ import java.util.List;
 * @return a raw listing of all Symptoms obtained from a data source duplicates are possible/probable
 */
 
+
 public interface ISymptomReader {
   List<String> getSymptoms() throws IOException;  
 }
